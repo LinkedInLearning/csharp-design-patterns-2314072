56d396312f6cd8ffb50c958a6463ac2974e9877a
Rich Goforth <rcgoforth@gmail.com>
2020-08-19 21:52:33 -0400

    04_02 Factory Sample

--- Factory/Program.cs
+++ Factory/Program.cs
@@ -6,7 +6,55 @@ namespace Factory
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var notificationServiceProvider = new NotificationServiceProvider();
+            var shippingService = new ShippingService(notificationServiceProvider);
+            shippingService.ShipItem();
+        }
+    }
+    interface IUserNotifier
+    {
+        void NotifyUser(int id);
+    }
+
+    class EmailUserNotifier : IUserNotifier
+    {
+        public void NotifyUser(int id)
+        {
+            Console.WriteLine($"Notified User {id} By Email");
+        }
+    }
+
+    class TestUserNotifier : IUserNotifier
+    {
+        public void NotifyUser(int id)
+        {
+            Console.WriteLine($"Pretending to notify User {id}");
+        }
+    }
+
+    class NotificationServiceProvider
+    {
+        public IUserNotifier GetUserNotifier()
+        {
+#if DEBUG
+            return new TestUserNotifier();
+#else
+            return new EmailUserNotifier();
+#endif
+        }
+    }
+
+    class ShippingService
+    {
+        NotificationServiceProvider _serviceProvider;
+        public ShippingService(NotificationServiceProvider serviceProvider)
+        {
+            _serviceProvider = serviceProvider;
+        }
+
+        public void ShipItem(){
+            //code to ship the item
+            _serviceProvider.GetUserNotifier().NotifyUser(1);
         }
     }
 }
