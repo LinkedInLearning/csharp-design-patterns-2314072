f58055b9e9f1c944651b6aaeb2809669d570a1ad
Rich Goforth <rcgoforth@gmail.com>
2020-08-20 18:41:00 -0400

    05_02 Create Sample Adapter

--- Adapter/Program.cs
+++ Adapter/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using Microsoft.Extensions.Logging;
+using System.Text;
+using System.IO;
 
 namespace Adapter
 {
@@ -6,7 +9,32 @@ namespace Adapter
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var logger = new FileLogger<Program>("Log.txt");
+            logger.LogDebug("This is a new log message");
+        }
+
+        class FileLogger<T> : FileStream, ILogger<T>
+        {
+            public FileLogger(string path) : base(path, FileMode.Append){}
+            
+            public IDisposable BeginScope<TState>(TState state)
+            {
+                throw new NotImplementedException();
+            }
+
+            public bool IsEnabled(LogLevel logLevel)
+            {
+                return true;
+            }
+
+            public void Log<TState>(LogLevel logLevel, EventId eventId, TState state, Exception exception, Func<TState, Exception, string> formatter)
+            {
+                byte[] messageByteArray = new UTF8Encoding(true)
+                    .GetBytes(state.ToString() + "\n");
+
+                Write(messageByteArray, 0, messageByteArray.Length);
+                Flush();
+            }
         }
     }
 }
