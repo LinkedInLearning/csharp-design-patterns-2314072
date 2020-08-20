044afa180750a572525fee6f6d3f468d5e9cd182
Rich Goforth <rcgoforth@gmail.com>
2020-08-20 18:18:11 -0400

    Create Adapter Project

---
+++ Adapter/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Adapter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
