f2f4ed8db84bfadf21e109a39ce449e3c2e8a77f
Rich Goforth <rcgoforth@gmail.com>
2020-08-19 21:17:22 -0400

    Formatting lines, adding data

--- HPlusSports/HPlusSports.Core/EnumerableExtensions.cs
+++ HPlusSports/HPlusSports.Core/EnumerableExtensions.cs
@@ -4,7 +4,10 @@ namespace HPlusSports.Core
 {
     public static class EnumerableExtensions
     {
-        public static IEnumerable<TResult> FilterdSelect<T, TResult>(this IEnumerable<T> enumerable, Func<T, bool> filter, Func<T, TResult> fieldSelector)
+        public static IEnumerable<TResult> FilterdSelect<T, TResult>(
+            this IEnumerable<T> enumerable, 
+            Func<T, bool> filter, 
+            Func<T, TResult> fieldSelector)
         {
             foreach (var a in enumerable)
                 if (filter(a)) yield return fieldSelector(a);            
